b1fef206b60ffc0572ba3cc6890fa39837f9720e
Chrystaly3 <josephine.nzioka@student.moringaschool.com>
2021-12-31 19:18:10 +0300

    adding styling

---
+++ index.js
@@ -0,0 +1,20 @@
+// let typed = new Typed(".typing",{
+// String:["Full Stack Developer"],
+// typeSpeed:120,
+// backSpeed:60,
+// loop:true
+
+// });
+var typed = new Typed(".typing",{
+	strings:["Full Stack Developer."],
+	typeSpeed:120,
+	backSpeed:60,
+	loop:true
+});
+
+var typed = new Typed(".typing2",{
+	strings:["Designer.","Frontend Developer."],
+	typeSpeed:120,
+	backSpeed:60,
+	loop:true
+});
